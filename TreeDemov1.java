f5dc61237f8c50ffc4ce2161583d938e0de94dda
LittleGroove <robbiemcgough@gmail.com>
2023-10-04 18:43:02 -0700

    Implemented find

--- TreeDemov1.java
+++ TreeDemov1.java
@@ -99,8 +99,13 @@ class Node{
 	   with a specific value
 	   */
 	   public boolean find(Node root, int key){
-		 //implement in here
-		  
+		if (root == null) {
+			return false;
+		}
+		if (root.value == key) {
+			return true;
+		}
+		return key < root.value ? find(root.left, key) : find(root.right, key);
 	   }
 	   
 	   
